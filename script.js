b90e83a207d7051c677c237f081663a8da04fefa
subbotindm <subbotin.95@gmail.com>
2024-10-19 10:45:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.querySelectorAll('.collapsible-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const content = button.nextElementSibling;
+function toggleContent(element) {
+    const content = element.nextElementSibling;
 
-        // Сворачиваем/разворачиваем блок
-        content.style.display = content.style.display === 'block' ? 'none' : 'block';
-    });
-});
+    // Сворачиваем/разворачиваем блок
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+}
